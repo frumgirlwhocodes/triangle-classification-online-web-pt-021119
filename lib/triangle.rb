8fba83b4adbbb9964709d392f6c0ee65923a2ceb
lieby weintraub <liebahinda@gmail.com>
2019-03-25 22:01:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def kind()
     if (a <= 0) || (b <= 0) || (c <= 0)
       raise TriangleError
-      (a+b) <= c || (b+C) <= a || (c + a) <= b
+     elsif (a+b) <= c || (b+C) <= a || (c + a) <= b
       raise TriangleError
       else 
         if (a == b) && (b == c) 
